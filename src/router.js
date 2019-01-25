3474d238dcf74a31bc905d0f82f6410025794d51
poppan <ggtrFc3d0>
2019-01-25 12:25:55 +0100

    route + /about restricted to logged-in users

--- src/router.js
+++ src/router.js
@@ -1,23 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+//export default new Router({
+const router = new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+            meta: {
+                requiresAuth: true
+            }
+        }
+    ],
+
+})
+export default router
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(routedeclaration => routedeclaration.meta.requiresAuth)) {
+        if (!firebase.auth().currentUser) {
+            next({
+                path: '/',
+                //query: {redirect: to.fullPath}
+            })
+        } else {
+            next() // ca passe
+        }
+    } else {
+        next() // hop
     }
-  ]
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,17 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+
+<script>
+  import firebase from 'firebase'
+  // @ is an alias to /src
+  export default {
+    name: 'about',
+    beforeCreate: function(){
+      if (!firebase.auth().currentUser){
+        this.$router.replace('/')
+      }
+    }
+  }
+</script>
